a7426225c52ebf560ce5a5acbf96b3651234db6d
Aurea Anguiano <11057966@uvu.edu>
2025-02-01 20:18:40 -0700

    bounce and color change

---
+++ AdventureGame/Assets/scripts/ColorChangeScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeScript : MonoBehaviour
+{
+    public Color newColor = Color.red;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //change the object's color upon callision with another GameObject
+        GetComponent<Renderer>().material.color = newColor;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
